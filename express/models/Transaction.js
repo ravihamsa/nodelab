487199b1e4015fa1ff0ffa65cac3c3b6760a3511
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2013-06-23 16:30:03 +0530

    updates

--- express/models/Transaction.js
+++ express/models/Transaction.js
@@ -10,24 +10,24 @@ var _ = require('underscore');
 var moment = require('moment');
 
 var TransactionSchema = new db.Schema({
-    state:String,
-    region:String,
-    district:String,
-    city:String,
-    language:String,
-    movieName:String,
-    theatre:String,
-    theatre_category:String,
-    screen:String,
-    show:String,
-    seating_type:String,
-    date:Date,
-    price:Number
+    state: String,
+    region: String,
+    district: String,
+    city: String,
+    language: String,
+    movieName: String,
+    theatre: String,
+    theatre_category: String,
+    screen: String,
+    show: String,
+    seating_type: String,
+    date: Date,
+    price: Number
 })
 
 
-TransactionSchema.methods.toString = function(){
-    return this.name+' '+this.producer;
+TransactionSchema.methods.toString = function () {
+    return this.name + ' ' + this.producer;
 }
 
 var MyTransaction = db.mongoose.model('Transaction', TransactionSchema);
@@ -36,72 +36,104 @@ module.exports.add = add;
 module.exports.all = all;
 module.exports.fillRecord = fillRecord;
 
-function add(obj, callback){
+function add(obj, callback) {
     var instance = new MyTransaction();
-    _.each(obj, function(value, key){
+    _.each(obj, function (value, key) {
         instance[key] = value;
     });
-    instance.save(function(err){
-        if(err){
+    instance.save(function (err) {
+        if (err) {
             callback(err);
-        }else{
+        } else {
             callback(null, instance);
         }
     })
 }
 
 
-function all(callback){
-    MyTransaction.find(function(err, transactions){
-        if(err){
+function all(callback) {
+    MyTransaction.find(function (err, transactions) {
+        if (err) {
             callback(err);
-        }else{
+        } else {
             callback(null, transactions);
         }
     });
 }
 
+function getRandom(max){
+    return Math.ceil(Math.random() * (max))
+}
 
-function fillRecord(callback){
+
+function fillRecord(callback) {
     //'state', 'regions', 'districts', 'cities',
-    var dimensions = ['language','movieName','theatre_category','theatre','screen','show','seating_type'];
+    var dimensions = ['language', 'movieName', 'theatre_category', 'theatre', 'screen', 'show', 'seating_type'];
     var indexCount = [6, 30, 3, 5, 2, 4, 3];
 
-    var date =  moment().startOf('day');
+    var date = moment().startOf('day');
 
-    for(var i= 0; i<30; i++){
+    for (var i = 0; i < 7; i++) {
         var obj = {};
-        for(var state=0; state<2; state++){
-            obj.state = 'state'+state;
-            for(var region=0; region<2; region++){
-                obj.region = 'region'+state+region;
-                for(var district=0; district<2; district++){
-                    obj.district = 'district'+state+region+district;
-                    for(var city=0; city<2; city++){
-                        obj.city = 'city'+state+region+district+city;
+        for (var state = 0; state < getRandom(2); state++) {
+            obj.state = 'state' + state;
+            for (var region = 0; region < getRandom(2); region++) {
+                obj.region = 'region' + state + region;
+                for (var district = 0; district < getRandom(2); district++) {
+                    obj.district = 'district' + state + region + district;
+                    for (var city = 0; city < getRandom(2); city++) {
+                        obj.city = 'city' + state + region + district + city;
 
-                        _.each(dimensions,function(key, index){
-                            obj[key] = key + Math.floor(Math.random()*(indexCount[index]));
-                        })
+                        for (var tc = 0; tc < getRandom(3); tc++) {
+                            var theatre_category = 'theatre_category' + tc;
 
-                        obj.price = 50*Math.ceil(Math.random()*(6));
-                        obj.date = date.valueOf();
-                        add(obj, function(err, instance){
-                            if(err){
-                                callback(err);
-                            }else{
-                                callback(null, instance);
+                            for (var t = 0; t < getRandom(3); t++) {
+                                var theater = 'theater' + t;
+                                var sc = getRandom(3)
+                                var screen = 'screen' + sc;
+                                var movieName = 'movieName'+ getRandom(10);
+                                var language = 'language'+ getRandom(5);
+                                for (var sh = 0; sh < 3; sh++) { //show
+                                    var show = 'show'+sh;
+                                    var price = (sh*sc*25)+25;
+
+                                    for(var st=0; st<2; st++){
+                                        var seating_type= 'seating_type'+st;
+                                        var sold = getRandom(10);
+                                        for(var ticket_count=0; ticket_count<sold; ticket_count++){
+                                            obj.price= price;
+                                            obj.show=show;
+                                            obj.screen=screen;
+                                            obj.theater=theater;
+                                            obj.theatre_category=theatre_category;
+                                            obj.seating_type=seating_type;
+                                            obj.movieName=movieName;
+                                            obj.language=language;
+                                            obj.date = date.valueOf();
+                                            add(obj, function (err, instance) {
+                                                if (err) {
+                                                    callback(err);
+                                                } else {
+                                                    callback(null, instance);
+                                                }
+                                            })
+                                        }
+                                    }
+
+
+
+                                }
                             }
-                        })
+                        }
+
+
                     }
                 }
             }
         }
 
-        date.subtract('days',1);
+        date.subtract('days', 1);
     }
-
-
 
 
 }
